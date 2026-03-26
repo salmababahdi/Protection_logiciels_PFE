7ed2a7f8721dc66d65755953cce60f2e0dd99241
Salmabhd <babahdisalma@gmail.com>
2026-03-26 11:55:54 +0000

    Initial commit: project setup with Gradle wrapper

---
+++ protetion_logiciels/src/main/java/com/example/protetion_logiciels/ProtetionLogicielsApplication.java
@@ -0,0 +1,14 @@
+package com.example.protetion_logiciels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ProtetionLogicielsApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtetionLogicielsApplication.class, args);
+	}
+
+}
